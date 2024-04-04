adcb96f54759ad94344c5eaf845d5ef556d561e7
unknown <you@example.com>
2024-04-04 17:54:38 +0500

    Adding a CLI based guessing game

---
+++ app.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer" ;
+
+const randomNumber = Math.floor(Math.random() * 10 + 1 ) ; 
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGussedNumber",
+        type: "number",
+        message: "Please Guess a number between 1 to 10: "
+    }
+]);
+
+if (answer.userGussedNumber === randomNumber){
+    console.log("Congratulations ! You Gussed The Right Number")
+}else{
+    console.log("Sorry ! You Gussed The Wrong Number")
+} ;
